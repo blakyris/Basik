63b42e5e8b1c29d78742cfd5dba23b62703193de
Sebastien Mercier <pro.sebastienmercier@gmail.com>
2017-12-08 18:33:32 +0100

    fix

--- config.sh
+++ config.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-############### VARIABLES ###############
-#SYSTEM GLOBAL CONFIGURATION
+# ---------------- CONFIGURATION VARIABLES -------------
+
+# SYSTEM GLOBAL CONFIGURATION
 HOSTNAME="archlinux"
 DOMAIN="local.dev"
 LOCALE="en_US.UTF-8"
 LANG="en_US.UTF-8"
 KEYMAP="fr"
 
-#USER SPECIFIC CONFIGURATION
+# USER SPECIFIC CONFIGURATION
 USERNAME="user"
 
 # SCRIPT VARIABLES
 
 
-#########################ä###############
+# ----------------------------------------------------
