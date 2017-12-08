c1ca2917590a8fc12a688b236e7bfb6f5a5e48e1
Sebastien Mercier <pro.sebastienmercier@gmail.com>
2017-12-08 16:53:45 +0100

    test

--- script.sh
+++ script.sh
@@ -20,19 +20,21 @@ chmod +x *.sh
 clear
 echo -e "******************** WARNING !! **********************\n"
 echo -e "Multiple partitions and dual boot is not supported yet."
-echo -e "This script erase all data on your primary hard drive !!"
+echo -e "This script erase all data on your primary hard drive (/dev/sda) !!"
 echo -e "Would you like to continue ? (Y/n) : "
 read ans
 if [ $ans == "Y" ] || [ $ans == "y" ] || [ $ans == "Yes" ] || [ $ans == "yes" ]
 then
   unset ans
+
   echo -e "You can customize your system settings before running the scripts"
   echo -e "Would you like to edit the configuration file ? (Y/n) : "
-  read ans2
-  if [ $ans2 == "Y" ] || [ $ans2 == "y" ] || [ $ans2 == "Yes" ] || [ $ans2 == "yes" ]
+  read ans
+  if [ $ans == "Y" ] || [ $ans == "y" ] || [ $ans == "Yes" ] || [ $ans == "yes" ]
   then
     nano config.sh
   fi
+
   ./install.sh
 else
   echo -e "Aborting..."
