f473e57936aa75ad99c8892b67f04e2134aaabec
Sebastien Mercier <pro.sebastienmercier@gmail.com>
2017-12-19 00:32:06 +0100

    added UI

--- chroot-script.sh
+++ chroot-script.sh
@@ -16,14 +16,50 @@ locale-gen
 echo LANG="$LANG" > /etc/locale.conf
 export LANG=$LANG
 echo KEYMAP="$KEYMAP" > /etc/vconsole.conf
+
+clear
+echo -e ""
+echo -e ">>  EDIT MKINITCPIO.CONF"
+echo -e "    --------------------"
+echo -e ""
+echo -e "You will now be able to edit mkinitcpio.conf in order to add \"lvm2\""
+echo -e "before \"filesystem\" in the HOOKS section. (this will be automatic"
+echo -e "in the future)."
+echo -e ""
+echo -n "Press [ENTER] to continue..."
+read ret
 emacs /etc/mkinitcpio.conf
 mkinitcpio -p linux
 grub-install --target=x86_64-efi --efi-directory=/boot/efi/ --bootloader-id=linux
 grub-mkconfig -o /boot/grub/grub.cfg
+
+clear
+echo -e ""
+echo -e ">>  ROOT PASSWORD"
+echo -e "    -------------"
+echo -e ""
+echo -e "You will now choose your root password"
+echo -e "Make sure it is VERY strong for your security !"
+echo -e ""
+echo -n "Press [ENTER] to continue..."
+read ret
+echo -e ""
 passwd
+
 groupadd admin
 useradd -g users -m -s /bin/bash $USERNAME
 usermod -aG admin $USERNAME
+echo -e ""
+echo -e ""
+echo -e ">>  MAIN USER PASSWORD"
+echo -e "    ------------------"
+echo -e ""
+echo -e "You will now choose your main user password ( $USERNAME )"
+echo -e "Make sure it is VERY strong for your security !"
+echo -e ""
+echo -n "Press [ENTER] to continue..."
+read ret
+echo -e ""
 passwd $USERNAME
 
 # Templates & Custom Scripts
